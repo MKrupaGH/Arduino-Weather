cd60b1f779bb79ffb7097cdfa7fd9c0b2241819a
Marek <marekkrupa03@gmail.com>
2022-11-13 21:44:36 +0100

    Added fetch method to get data

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,47 +1,25 @@
 import { getWeatherData } from "./dataOperations/getData";
-//import { format, formatDistance, formatRelative, subDays } from "date-fns";
 import moment from "moment-timezone";
+
 export function viewPage() {
-  const typeW = document.querySelector(".type");
   const city = document.querySelector(".city");
   const flag = document.querySelector(".flag-img");
   const temp = document.querySelector(".temp");
   const hour = document.querySelector(".hour");
-  const tempF = document.querySelector(".tempF");
   const hum = document.querySelector(".hum");
   const press = document.querySelector(".press");
-  const wind = document.querySelector(".wind");
   const clouds = document.querySelector(".clouds");
-  const btn = document.querySelector("button");
-  const inputValue = document.querySelector("#city");
-  const errorS = document.querySelector(".error");
 
   const info = document.querySelector(".info");
-
-  const form = document.querySelector("form");
 
   (function startSetUp() {
     animation();
     getObj();
   })();
 
-  (function addListener() {
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      animation();
-      getObj();
-      clear();
-    });
-  })();
-
   function getObj() {
-    getWeatherData(inputValue.value).then((response) => {
-      if (response.status === "success") {
-        errorS.textContent = response.message;
-        getNeedData(response);
-      } else {
-        errorS.textContent = response.message;
-      }
+    getWeatherData().then((response) => {
+      getNeedData(response);
     });
   }
 
@@ -49,60 +27,36 @@ export function viewPage() {
     const obj = {
       flag: response.flag.url,
       city: response.data.name,
-      timezone: response.data.timezone,
-      temp: response.data.main.temp,
-      weatherType: response.data.weather[0].description,
-      fellTemp: response.data.main["feels_like"],
-      hum: response.data.main.humidity,
-      pressure: response.data.main.pressure,
-      wind: response.data.wind.speed,
-      clouds: response.data.clouds.all,
+      time: response.newestMongo.data[0].createdAt,
+      temp: response.newestMongo.data[0].temp,
+      hum: response.newestMongo.data[0].hum,
+      pressure: response.newestMongo.data[0].pres,
+      clouds: response.newestMongo.data[0].clo,
     };
     processData(obj);
   }
 
   function processData(result) {
-    const timezone = result.timezone;
-    const timezoneMin = timezone / 60;
-    const timeView = moment()
-      .utcOffset(timezoneMin)
-      .format("DD/MM/YYYY, h:mm A");
-    const tempC = Math.round(result.temp, 3);
-    const tempCFell = Math.round(result.fellTemp, 3);
+    const time = new Date(result.time);
+    const lastTime = moment(time).format("DD/MM/YYYY, h:mm A");
 
-    const tempF = (tempC * 9) / 5 + 32;
-    const tempFFell = (tempCFell * 9) / 5 + 32;
-
-    const tempCStr = `${tempC} \xB0C`;
-    const tempCFellStr = `${tempCFell} \xB0C`;
-
-    const tempFStr = `${tempF} \xB0F`;
-    const tempFFellStr = `${tempFFell} \xB0C`;
+    const tempCStr = `${result.temp} \xB0C`;
 
     const hum = `${result.hum} %`;
 
     const pressure = `${result.pressure} hPa`;
-
-    const wind = `${result.wind} m/s`;
 
     const clouds = `${result.clouds} %`;
 
     const weatherInfo = {
       flag: result.flag,
       city: result.city,
-      weather: result.weatherType,
       date: {
-        timeView,
+        lastTime,
       },
-      temp: {
-        tempCStr,
-        tempFStr,
-        tempCFellStr,
-        tempFFellStr,
-      },
+      temp: tempCStr,
       hum,
       pressure,
-      wind,
       clouds,
     };
 
@@ -110,20 +64,13 @@ export function viewPage() {
   }
 
   function showWeather(obj) {
-    typeW.textContent = obj.weather;
     city.textContent = obj.city + ", ";
     flag.src = obj.flag;
-    hour.textContent = obj.date.timeView;
-    temp.textContent = obj.temp.tempCStr;
-    tempF.textContent = obj.temp.tempCFellStr;
+    hour.textContent = obj.date.lastTime;
+    temp.textContent = obj.temp;
     hum.textContent = obj.hum;
     press.textContent = obj.pressure;
-    wind.textContent = obj.wind;
     clouds.textContent = obj.clouds;
-  }
-
-  function clear() {
-    form.reset();
   }
 
   function animation() {
--- src/modules/dataOperations/getData.js
+++ src/modules/dataOperations/getData.js
@@ -1,26 +1,25 @@
-export async function getWeatherData(city) {
-  let htmlRequest;
-  if (city === "") {
-    htmlRequest = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&units=metric&APPID=a90a094d8ba09340beba0e7fd95a30fa`;
-  } else {
-    htmlRequest = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=a90a094d8ba09340beba0e7fd95a30fa`;
-  }
+export async function getWeatherData() {
+  let htmlRequest = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&units=metric&APPID=a90a094d8ba09340beba0e7fd95a30fa`;
+
   try {
     const dataJSON = await fetch(htmlRequest, { mode: "cors" });
+    const dataMongo = await fetch(
+      "https://shocking-coffin-88092.herokuapp.com/catalog/values/newest",
+      { mode: "cors" }
+    );
 
     const data = await dataJSON.json();
 
+    const newestMongo = await dataMongo.json();
     const flag = await fetch(
       `https://countryflagsapi.com/png/${data.sys.country}`,
       { mode: "cors" }
     );
-    return { status: "success", message: "", data, flag };
+    return { status: "success", message: "", data, flag, newestMongo };
   } catch (error) {
     return {
       status: "failed",
-      message: "No matching location found",
-      data: null,
-      flag: null,
+      message: "Error on fetching",
     };
   }
 }
